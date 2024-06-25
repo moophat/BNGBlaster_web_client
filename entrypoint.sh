1d9d6314ea1b6aea15c8dea829d6f7cb9b394afe
Nik <59143079+linhnt-hub@users.noreply.github.com>
2024-06-25 15:39:00 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # mkdir -p $DB_DIR
-streamlit run HOME.py --server.port=8501 --server.baseUrlPath=/blaster/ --server.address=0.0.0.0
+streamlit run HOME.py --server.port=8505 --server.address=0.0.0.0
